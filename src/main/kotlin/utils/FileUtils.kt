fb9f6bab95aec72e6b710c602b2cd1c3d1a887de
Jean Lainee <jean.lainee@gmail.com>
2025-05-17 22:16:47 +0800

    Fix file output path

--- src/main/kotlin/utils/FileUtils.kt
+++ src/main/kotlin/utils/FileUtils.kt
@@ -5,7 +5,10 @@ import java.io.File
 class FileUtils {
 
     companion object {
-        fun getFileWithNewExtension(file: File, newExtension: String) = File("${file.parentFile.absolutePath}/${file.nameWithoutExtension}${newExtension}")
+        fun getFileWithNewExtension(file: File, newExtension: String) = when(file.isFile) {
+            true -> File("${file.parentFile.absolutePath}/${file.nameWithoutExtension}${newExtension}")
+            false -> File("${file.parentFile.absolutePath}/out${newExtension}")
+        }
     }
 
 }
